eae6b64a7cc4f3e01b5ae5acdb10c703e46866f5
Liam Magee <liam.magee@gmail.com>
2013-11-29 11:00:51 +1100

    Updating Universe export

--- sample-app.js
+++ sample-app.js
@@ -2,13 +2,13 @@
 // 3rd party dependencies
 var gauss = require('gauss');
 var _ = require('underscore');
-require('./lib/fp-core');
 
 var fp = require('./lib/models/index');
+var Universe = fp.Universe;
 var Lifecycle = fp.Lifecycle;
 var Module = fp.Module;
 var DefaultModule = require('./lib/default-module/default-module').DefaultModule;
-var Universe = require('./lib/models/universe').Universe;
+
 
 
 var a;
